c7f2d66a52e43eeeb67fc3f102d4577d10f81578
Robert Kuznetsov <pulflim3@icloud.com>
2022-05-15 12:58:31 +0300

    123

---
+++ .vs/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <string>
+using namespace std;
+class cat {
+public:
+	string noise;
+};
+class dog {
+public:
+	string noise;
+};
+int main() {
+	cat firstcat;
+	dog firstdog;
+	firstcat.noise = "meuuw";
+	firstdog.noise = "gav gav";
+	cout << firstdog.noise << endl;
+	cout << firstcat.noise << endl;
+	return 0;
+}
+  
+
