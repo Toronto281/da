da62476b63b49a632e28c53dba92ce4f8072813c
Robert Kuznetsov <pulflim3@icloud.com>
2022-05-20 12:16:41 +0300

    for brat

---
+++ fot brat.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class cat {
+public:
+	string noise;
+	
+};
+class dog {
+public:
+	string noise;
+};
+int main() {
+	dog firstdog;
+	cat firstcat;
+	
+	cin >> firstcat.noise;
+	cin >> firstdog.noise;
+	cout << firstdog.noise << endl;
+	cout << firstcat.noise << endl;
+	return 0;
+}
+
