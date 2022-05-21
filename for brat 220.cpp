3bac42353a77b35f225f1d4466c06419d8e9b463
Robert Kuznetsov <pulflim3@icloud.com>
2022-05-21 12:41:48 +0300

    222

---
+++ for brat 220.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+int fib(int n)
+{
+	if (n < 1) {
+		return 0;
+	}
+	if (n == 1) {
+		return 1;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+void main() {
+	int res = fib(9);
+	cout << res << endl;
+}
+
