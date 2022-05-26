b5062594428134268e8ed40ce68978ef15779dd0
Robert Kuznetsov <pulflim3@icloud.com>
2022-05-26 16:55:33 +0300

    999

---
+++ for brat 220.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int fib[100];
+	int n;
+	n = 4;
+	fib[0] = 1;
+	fib[1] = 1;
+	for (int i = 2; i < n; ++i) {
+		fib[i] = fib[i - 1] + fib[i - 2];
+	}
+	for (int i = 0; i < n; i++) {
+		cout << fib[i] << " " << endl;
+	}
+	
+}
+
+
+
